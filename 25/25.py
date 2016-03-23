8c38f1161e99ab6b40682aec8d42aa3a3bcff956
D-e-e-m-o <wsd009811@gmail.com>
2016-03-23 20:48:59 +0800

    改好后貌似没什么问题的二维数组版本

--- 25/25.py
+++ 25/25.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
 
-cin = open('input','r')
+cin = open('input', 'r')
 values = {}
 for i in range(15):
 	weight = int(cin.readline())
-	values[i] = [weight, weight]
+	values[i+1] = [weight, weight]
 f = []
-for i in range(15):
-	f.append([0 for j in range(5000)])
-for i in range(15):
-	for j in range(5000):
+for i in range(16):
+	f.append([0 for j in range(5001)])
+for i in range(1, 16):
+	for j in range(1, 5001):
 		if j-values[i][0] >= 0:
 			f[i][j] = max(f[i-1][j], f[i-1][j-values[i][0]]+values[i][1])
 		else:
 			f[i][j] = f[i-1][j]
-print(f[14][4999])
+print(f[15][5000])
 
-i = 14
-j = 4999
-while i >= 0 and j >= 0:
+i = 15
+j = 5000
+while i > 0 and j >= 0:
 	if j - values[i][0] >= 0 and f[i][j] == f[i - 1][j - values[i][0]] + values[i][1]:
-		print([i+1, values[i][0]])
+		print([i, values[i][0]])
 		j -= values[i][0]
 	i -= 1
-
